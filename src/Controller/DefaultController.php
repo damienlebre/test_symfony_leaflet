fb99f6c692853149deac0ad62f5ad48f6d7300ce
Damien <damien.lebre88@gmail.com>
2022-05-18 16:29:52 +0200

    second commit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ZipType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index( Request $request): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+        $form = $this->createForm(ZipType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values       
+
+            // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('default/index.html.twig');
+        }
+
+        return $this->renderForm('default/index.html.twig', [
+            'form' => $form,
         ]);
     }
 }
--- src/Form/ZipType.php
+++ src/Form/ZipType.php
@@ -4,14 +4,35 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class ZipType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('Zip',  IntegerType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez un code postal a rechercher'
+                    ])
+                ]
+            ])
+            ->add('city',  TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez un nom de localisation a rechercher '
+                    ])
+                ]
+            ])
+            ->add('Filtrer', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-secondary m-3']
+            ])
+            ;
         ;
     }
 
